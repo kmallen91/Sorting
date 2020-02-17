44a7a83b1b8e1c538b30d3475982faaebfaf1033
Kevin Allen <kevin.allen91@gmail.com>
2020-02-17 14:31:53 -0800

    binary search complete

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -17,14 +17,24 @@ print(linear_search([2, 5, -1, 66, 7], 2))
 def binary_search(arr, target):
 
     if len(arr) == 0:
-        return -1  # array empty
+        return 'Array is empty'  # array empty
 
     low = 0
     high = len(arr)-1
-
     # TO-DO: add missing code
+    while low < high:
+        mid = (low + high) // 2
+        if target > mid:
+            low = mid + 1
+        elif target < mid:
+            high = mid - 1
+        else:
+            return mid, True
 
-    return -1  # not found
+    return 'Item not in array'  # not found
+
+
+print(binary_search([0, 2, 3, 4, 5, 8, 10, 13, 15, 17, 19, 22], 20))
 
 
 # STRETCH: write a recursive implementation of Binary Search
